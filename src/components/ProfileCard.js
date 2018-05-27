272482376575bd0ef4214af9152c8f7ad00928b6
Nate Dunn <natedunn@Damariss-MacBook-Pro.local>
2018-05-27 16:57:47 -0500

    Clean up spacing

--- src/components/ProfileCard.js
+++ src/components/ProfileCard.js
@@ -8,12 +8,12 @@ class ProfileCard extends React.Component {
     };
     return (
       <div className="bg-white br2 pa4" style={styles}>
-        <h3 className="dib w-100">Username</h3>
+        <h3 className="dib w-100 mt0 mb1">Username</h3>
         <span className="dib w100">@username</span>
-        <div className="flex flex-column">
+        <div className="flex flex-column mt3">
           <span className="pa2 bg-near-white mb2">Lists: 8</span>
           <span className="pa2 bg-near-white mb2">Following: 123</span>
-          <span className="pa2 bg-near-white mb2">Followers: 198</span>
+          <span className="pa2 bg-near-white mb0">Followers: 198</span>
         </div>
       </div>
     );
