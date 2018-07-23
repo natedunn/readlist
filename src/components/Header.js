3d652ae8b7d142d1b79c22481693d042ef179778
Nate Dunn <hello@natedunn.net>
2018-07-23 12:53:14 -0700

    Add new header content

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,19 +4,25 @@ import { Link } from "react-router-dom";
 class Header extends React.Component {
   render() {
     return (
-      <header className="bb b--black-30 bg-white">
-        <div className="mw8 ml-auto mr-auto pa3">
-          <b className="dib w-50">
-            <Link to={`/`}>Readlists.io</Link>
-          </b>
-          <nav className="dib w-50">
-            <ul className="tr">
-              <li className="list dib mr3">Home</li>
-              <li className="list dib mr3">Profile</li>
-              <li className="list dib">Settings</li>
-            </ul>
-          </nav>
-        </div>
+      <header className="mw6 ml-auto mr-auto mt3 bb b--blue">
+        <nav className="flex pt3 pb3">
+          <Link className="flex-auto dib b" to={`/`}>
+            Readlist
+          </Link>
+          <ul className="flex-auto tr pl0 mv0 list">
+            <li className="dib mr3">
+              <Link to={`/`}>Options</Link>
+            </li>
+            <li className="dib mr3">
+              <Link to={`/`}>Sign Out</Link>
+            </li>
+            <li className="dib mr3">
+              <Link className="bg-blue pv1 ph2 white no-underline" to={`/`}>
+                Create List
+              </Link>
+            </li>
+          </ul>
+        </nav>
       </header>
     );
   }
