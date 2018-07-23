c2d5dd94f647b8b3baeae065548beb9dc97936b1
Nate Dunn <hello@natedunn.net>
2018-07-23 12:54:18 -0700

    Add theme styles

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { render } from "react-dom";
 import Router from "./components/Router";
+import "./theme.css";
 
 render(<Router />, document.getElementById("main"));
